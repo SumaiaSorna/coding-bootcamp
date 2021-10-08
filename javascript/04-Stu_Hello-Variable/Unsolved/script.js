aff9a4350175e6fe581639f5c006d88950328829
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-08 15:46:44 +0100

    solved activity 04

--- javascript/04-Stu_Hello-Variable/Unsolved/script.js
+++ javascript/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,16 @@
 // Write Your JavaScript Code Here
+var personName = "Sumaia";
+var pets = 1;
+var funFact = "has lot more in Bangladesh";
+
+console.log("My name is " + personName);
+console.log("I have " + pets + " pets");
+console.log("funFact " + funFact);
+
+personName = "Sorna";
+pets = 5;
+funFact = "want to have more";
+
+console.log("My name is " + personName);
+console.log("I have " + pets + " pets");
+console.log("funFact " + funFact);
