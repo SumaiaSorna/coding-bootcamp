783f199f3740fa12c6aebbe49e04e607159c82cf
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-08 16:29:32 +0100

    solved activity 06

--- javascript/06-Stu_Primitive-Types/Unsolved/script.js
+++ javascript/06-Stu_Primitive-Types/Unsolved/script.js
@@ -1,23 +1,33 @@
 var one = 64;
-var two = '656302';
+var two = "656302";
 var three = false;
 var four = 64.55;
-var five = 'Howdy!';
+var five = "Howdy!";
 var six;
 
 // Insert comments to explain what each console log below will log to the console
+
+//Type of Number
 console.log(typeof one);
+//Type of string
 console.log(typeof two);
+//Boolean
 console.log(typeof three);
+//Number
 console.log(typeof four);
+//String
 console.log(typeof five);
+//Null
 console.log(typeof six);
 
-four = 'Hello!';
+four = "Hello!";
 five = false;
 six = 23;
 
 // Insert comments to explain what each console log below will log to the console
+//String
 console.log(typeof four);
+//Boolean
 console.log(typeof five);
+//Number
 console.log(typeof six);
