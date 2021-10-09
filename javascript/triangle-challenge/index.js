e684483e83095100587fbc388ce0c85e75171657
SumaiaSorna <sorna.sumaia@gmail.com>
2021-10-09 09:50:10 +0100

    solved triangle challenge

--- javascript/triangle-challenge/index.js
+++ javascript/triangle-challenge/index.js
@@ -1,81 +1,26 @@
-const isValidTriangle = function (side1, side2, side3) {
-  if (
-    side1 + side2 > side3 &&
-    side2 + side3 > side1 &&
-    side3 + side1 > side2 &&
-    side1 > 0 &&
-    side2 > 0 &&
-    side3 > 0
-  ) {
-    return true;
+// defining variable
+let side1;
+let side2;
+let side3;
+let x = side1 + side2;
+let y = side2 + side3;
+let z = side3 + side1;
+
+function triangle() {
+  if (side1 === side2 && side2 === side3) {
+    console.log("Equilateral");
+  } else if (side1 != side2 && side2 != side3 && side3 != side1) {
+    console.log("scalene");
   } else {
-    return false;
+    console.log("isosceles");
   }
-};
+  return;
+}
 
-const isEquilateral = function (side1, side2, side3) {
-  if (side1 === side2 && side2 === side3 && side3 === side1) {
-    return true;
-  } else {
-    return false;
-  }
-};
+//lets define if it is a triangle
 
-const isIsosceles = function (side1, side2, side3) {
-  if (side1 === side2 || side2 === side3 || side3 === side1) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const isScalene = function (side1, side2, side3) {
-  if (side1 !== side2 && side2 !== side3 && side3 !== side1) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const triangle = function (side1, side2, side3) {
-  // check if all 3 sides are valid
-
-  if (isValidTriangle(side1, side2, side3)) {
-    // check if equilateral
-    if (isEquilateral(side1, side2, side3)) {
-      console.log('EQUILATERAL Triangle');
-      return;
-    }
-
-    // check if isosceles
-    if (isIsosceles(side1, side2, side3)) {
-      console.log('ISOSCELES Triangle');
-      return;
-    }
-
-    // check if scalene
-    if (isScalene(side1, side2, side3)) {
-      console.log('SCALENE Triangle');
-      return;
-    }
-  } else {
-    // display message
-    console.log('Is not a triangle');
-    return;
-  }
-};
-
-// scalene triangle
-triangle(6, 8, 10);
-
-// equilateral triangle
-triangle(6, 6, 6);
-
-// isosceles triangle
-triangle(6, 6, 10);
-
-// not a triangle
-triangle(6, 80, 10);
-
-// not a triangle
-triangle(-6, -80, 10);
+if (side1 && side2 && side3 > 0 && x > side3 && y > side1 && z > side2) {
+  triangle();
+} else {
+  console.log("Its not a triangle");
+}
