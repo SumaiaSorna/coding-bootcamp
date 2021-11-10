db56beefa4ad5af0253d644ad4c03d5d7df24356
SumaiaSorna <sorna.sumaia@gmail.com>
2021-11-10 10:22:11 +0000

    challenge-1 in advanced-js done

--- advanced-js/challenge-1.js
+++ advanced-js/challenge-1.js
@@ -1,24 +1,32 @@
 // function to create initials for a full name
 
-const createInitials = (fullName) => {
-  // remove white spaces and convert to uppercase and convert str to arr
-  const fullNameArray = fullName.trim().toUpperCase().split(" ");
-  console.log(fullNameArray);
+// const createInitials = (fullName) => {
+//   // remove white spaces and convert to uppercase and convert str to arr
+//   const fullNameArray = fullName.trim().toUpperCase().split(" ");
+//   console.log(fullNameArray);
 
-  // get first name from the array
-  const firstName = fullNameArray[0];
+//   // get first name from the array
+//   const firstName = fullNameArray[0];
 
-  // get last name from the array
-  const lastName = fullNameArray.pop();
+//   // get last name from the array
+//   const lastName = fullNameArray.pop();
 
-  const firstNameChar = firstName.charAt(0);
-  const lastNameChar = lastName.charAt(0);
+//   const firstNameChar = firstName.charAt(0);
+//   const lastNameChar = lastName.charAt(0);
 
-  console.log(firstNameChar);
-  console.log(lastNameChar);
+//   console.log(firstNameChar);
+//   console.log(lastNameChar);
 
-  return "B.S";
-};
+//   return `${firstNameChar}.${lastNameChar}`;
+// };
+
+const createInitials = (fullName) =>
+  fullName
+    .trim()
+    .toUpperCase()
+    .split(" ")
+    .map((each) => each.charAt(0))
+    .join(".");
 
 // Happy path test 1
 console.log(createInitials("Bob Smith"));
