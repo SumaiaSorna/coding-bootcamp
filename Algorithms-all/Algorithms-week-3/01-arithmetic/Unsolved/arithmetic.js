5471641c6a2165368c5b82c074ffa0c1172536de
SumaiaSorna <sorna.sumaia@gmail.com>
2021-12-03 16:56:59 +0000

    solved week 3 and week 5 algorithym

--- Algorithms-all/Algorithms-week-3/01-arithmetic/Unsolved/arithmetic.js
+++ Algorithms-all/Algorithms-week-3/01-arithmetic/Unsolved/arithmetic.js
@@ -1,9 +1,33 @@
 // Fill in the body of each function with the code required
 
-var add = function (num1, num2) {};
+// const add = function (num1, num2) {
+//   return num1 + num2;
+// };
 
-var subtract = function (num1, num2) {};
+// const subtract = function (num1, num2) {
+//   return num1 - num2;
+// };
 
-var multiply = function (num1, num2) {};
+// const multiply = function (num1, num2) {
+//   return num1 * num2;
+// };
 
-var divide = function (num1, num2) {};
+// const divide = function (num1, num2) {
+//   return num1 / num2;
+// };
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
--- Algorithms-all/Week-04-Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
+++ Algorithms-all/Week-04-Algorithms/01-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,10 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function (num) {};
+const logEvenNums = (nums) => {
+  for (let i = 0; i <= nums; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+};
